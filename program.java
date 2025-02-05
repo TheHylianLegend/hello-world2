e619231467f2099b28dd38316c58fbd4d73da78a
TheHylianLegend <ohp2004@gmail.com>
2025-02-05 14:19:45 -0500

    Merged to Denise Branch

--- program.java
+++ program.java
@@ -6,6 +6,7 @@ public class program {
         System.out.println("Hello world!");
         System.out.println("Hi Alice");
         System.out.println("Hi Bob!");
+        System.out.println("Hi Charlie!");
         System.out.println("Goodbye! :)");
     }
 }
