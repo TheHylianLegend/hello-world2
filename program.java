0c68d695358a4b575100b59ed57528f1fd9ae0ef
TheHylianLegend <ohp2004@gmail.com>
2025-02-05 14:09:36 -0500

    Fixed Alice Branch

--- program.java
+++ program.java
@@ -2,9 +2,10 @@
 public class program {
 
     public static void main(String[] args) {
+        System.out.println("Here ye : important announcement");
         System.out.println("Hello world!");
         System.out.println("Hi Alice");
-
+        System.out.println("Goodbye! :)");
     }
 
 }
