55378211bb8f2298323bdfed5e6af56b3819b1c9
TheHylianLegend <ohp2004@gmail.com>
2025-02-05 12:38:10 -0500

    Added preamble

--- program.java
+++ program.java
@@ -2,6 +2,7 @@
 public class program {
 
     public static void main(String[] args) {
+        System.out.println("Here ye : important announcement");
         System.out.println("Hello world!");
 
     }
