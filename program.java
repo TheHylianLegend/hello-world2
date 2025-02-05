5e42ecd9004e747f6256b8599914e3203d0486f1
TheHylianLegend <ohp2004@gmail.com>
2025-02-05 14:03:16 -0500

    Added Bob Greeting

--- program.java
+++ program.java
@@ -4,6 +4,7 @@ public class program {
     public static void main(String[] args) {
         System.out.println("Here ye : important announcement");
         System.out.println("Hello world!");
+        System.out.println("Hi Bob!");
         System.out.println("Goodbye! :)");
 
     }
