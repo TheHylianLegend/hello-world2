68b01f0f43c27ec9b77536ca75acd3b23d569016
TheHylianLegend <ohp2004@gmail.com>
2025-02-05 14:18:04 -0500

    Fixed Denise Branch

--- program.java
+++ program.java
@@ -6,7 +6,6 @@ public class program {
         System.out.println("Hello world!");
         System.out.println("Hi Alice");
         System.out.println("Hi Bob!");
-        System.out.println("Hi Charlie!");
         System.out.println("Goodbye! :)");
     }
 
