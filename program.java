11c4a19ef2b50bb66edc917cf84826315a7c0f07
TheHylianLegend <ohp2004@gmail.com>
2025-02-12 16:51:12 -0500

    Implemented free memory string

--- program.java
+++ program.java
@@ -9,9 +9,16 @@ public class program {
         DateTimeFormatter format = DateTimeFormatter.ofPattern("MM-dd-yyyy");
         String text = curDate.format(format);
         
+        Runtime rt = Runtime.getRuntime();
+        long freeMemory;
+        
+        
         System.out.println("Here ye : important announcement");
         System.out.println("Hello world!");
         System.out.println("Todays date is " + text);
+        freeMemory = rt.freeMemory();
+        System.out.println("Current free memory: " + freeMemory);
+        
         System.out.println("Goodbye! :)");
     }
 }
